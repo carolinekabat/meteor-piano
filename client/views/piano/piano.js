0046289a40175e769a854ac4514633eab0dc323c
Gustav Svalander <gsvalander@gmail.com>
2015-05-14 20:18:37 +0200

    pubsub

--- client/views/piano/piano.js
+++ client/views/piano/piano.js
@@ -1,12 +1,10 @@
 Template.piano.onCreated(function() {
-  this.subscribe('tones');
+  //this.subscribe('tones');
 });
 
 Template.piano.events({
   'click .key': function(event, instance) {
-    console.log("Pressed key "+$(event.target).data("tone")+"!");
-
-    // play sound
+    console.log("Inserting sound "+$(event.target).data("tone"));
 
     Tones.insert({
       sound: $(event.target).data("tone")
